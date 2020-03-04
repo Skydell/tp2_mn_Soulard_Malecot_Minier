f83b94e1c4d388d4632d3f841cb263610cc1d65d
Alexandre Soulard <alexandre.soulard@etu.univ-grenoble-alpes.fr>
2020-03-04 14:56:24 +0100

    Amelioration complexe2.h

--- include/complexe2.h
+++ include/complexe2.h
@@ -52,34 +52,36 @@ inline complexe_double_t mult_complexe_double (const complexe_double_t c1, const
 
 inline complexe_float_t div_complexe_float (const complexe_float_t c1, const complexe_float_t c2)
 {
-	complexe_float_t r ;
-	
-	complexe_float_t conjc2;
-	conjc2.real = c2.real;
-	conjc2.imaginary = -c2.imaginary;
-	
-	conjc2 = mult_complexe_float(c1, conjc2);
+  complexe_float_t c2conjug;
 
-	r.real = conjc2.real;
-	r.imaginary = conjc2.imaginary;
-  
-	return r ;
+  c2conjug.real = c2.real;
+  c2conjug.imaginary = (-1.0)*c2.imaginary;
+
+  complexe_float_t resinter = mult_complexe_float(c1, c2conjug);
+  complexe_float_t resinter2 = mult_complexe_float(c2, c2conjug);
+
+  complexe_float_t res;
+  res.real = resinter.real/resinter2.real;
+  res.imaginary = resinter.imaginary/resinter2.real;
+
+  return res;
 }
 
 inline complexe_double_t div_complexe_double (const complexe_double_t c1, const complexe_double_t c2)
 {
-	complexe_double_t r ;
-	
-	complexe_double_t conjc2;
-	conjc2.real = c2.real;
-	conjc2.imaginary = -c2.imaginary;
-	
-	conjc2 = mult_complexe_double(c1, conjc2);
+  complexe_double_t c2conjug;
 
-	r.real = conjc2.real;
-	r.imaginary = conjc2.imaginary;
-  
-	return r ;
+  c2conjug.real = c2.real;
+  c2conjug.imaginary = (-1.0)*c2.imaginary;
+
+  complexe_double_t resinter = mult_complexe_double(c1, c2conjug);
+  complexe_double_t resinter2 = mult_complexe_double(c2, c2conjug);
+
+  complexe_double_t res;
+  res.real = resinter.real/resinter2.real;
+  res.imaginary = resinter.imaginary/resinter2.real;
+
+  return res;
 }
 
 
