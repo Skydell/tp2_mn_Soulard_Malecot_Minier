4883c82840175ac7684c38e77d2d6318b37f61a8
Titouan Minier-Mancini <titouan.minier-mancini@etu.univ-grenoble-alpes.fr>
2020-05-20 14:37:10 +0200

    Mise a jour Asum

--- src/asum.c
+++ src/asum.c
@@ -1,5 +1,5 @@
-#include "mnblas.h"
-#include "complexe2.h"
+#include "../include/mnblas.h"
+#include "../include/complexe2.h"
 
 float cblas_sasum (const int n, const float *x, const int incx) {
 	if (n <= 0 || incx <= 0) return 0;
@@ -19,22 +19,22 @@ double cblas_dasum (const int n, const double *x, const int incx) {
 	return res;
 }
 
-void cblas_casum (const int n, const void *x, const int incx) {
+float cblas_casum (const int n, const complexe_float_t *x, const int incx) {
 	if (n <= 0 || incx <= 0) return 0;
 	float res = 0;
 	for (int i = 0; i < n; i+=incx) {
-		res+=x[i]->real;
-		res+=x[i]->imaginary;
+		res+=x[i].real;
+		res+=x[i].imaginary;
 	}
 	return res;
 }
 
-void cblas_zasum (const int n, const void *x, const int incx) {
+double cblas_zasum (const int n, const complexe_double_t *x, const int incx) {
 	if (n <= 0 || incx <= 0) return 0;
 	double res = 0;
 	for (int i = 0; i < n; i+=incx) {
-		res+=x[i]->real;
-		res+=x[i]->imaginary;
+		res+=x[i].real;
+		res+=x[i].imaginary;
 	}
 	return res;
 }
